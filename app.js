ca893ba135faddd3b5feed9dc8c6216cad8d461c
Ian IJmkers <ianymkers@gmail.com>
2023-10-06 20:06:17 +0200

    I had some issues before with mime and linking to other html pages when i realized i had a terminal issue whereby I am unable to type when im running node app.js, only when i CTRL + C. Albeit I was able to overcome to the situation and essentially link the pages. Now its just simple adding to the html files

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const PORT = 5000;
 
 //All the routes here
 app.get("/nikis_best", (req, res) => {
     res.sendFile(__dirname + '/views/niki.html')
+});
+
+app.get("/nikisFacts", (req, res) => {
+    res.sendFile(__dirname + '/views/facts.html')
+});
+
+app.get("/nikisPortfolio", (req, res) => {
+    res.sendFile(__dirname + '/views/portfolio.html')
 });
 
 //First thing is to listen to a port and start the server
